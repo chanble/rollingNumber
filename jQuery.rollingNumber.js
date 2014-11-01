f6cd24537fe5339f2e6eccde58c4d29ec124bd77
chanble <chanbo.cn@gamil.com>
2014-11-01 14:20:29 +0800

    add init element

--- jQuery.rollingNumber.js
+++ jQuery.rollingNumber.js
@@ -7,11 +7,10 @@
 (function($){
 	$.fn.rollingNumber = function(opts){
 		$.each(this, function (i, v){
-			var input = v;
-			new auto_complete(input,opts);
+			new auto_complete(v,opts);
 		});
-	}
-	var RollingNumber = function(){
+	};
+	var RollingNumber = function(el, opts){
 		var that = this,
 		defaultOpts = {
 			'hight' : '22',
@@ -19,6 +18,26 @@
 			'dataType' : 'url',//url ,local
 			'url' : 'http://www.youwebsite.com/api/',//url ,local
 			'intervalTime' : '3',//间隔时间
+			'numberSize' : '3',//数字位数
 		},
-	}
+		options = $.extends(false,defaultOpts, opts);
+		this.init(el, options);
+	},
+	RollingNumber.protetype = {
+		init: function(el, options){
+
+
+		},
+		_initAllNumber: function(numberSize){
+			var numbers = new array();
+			for(var i = 0; i < numberSize; i++){
+				numbers.push(this._initNumber());
+			}
+			return numbers;
+		},
+		_initNumber: function(){
+			var bitEl = $('<span class="rollingNumber_bit"><div class="rollingNumber_items" style="position:relative;"></div></span>');
+			return bitEl;
+		}
+	};
 })(jQuery);
