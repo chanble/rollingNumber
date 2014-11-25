534afdc6f9b4e208f2f64ffe13617ccbe7c22489
chanble <chanbo.cn@gamil.com>
2014-11-25 22:26:34 +0800

    添加了可以设置数字

--- jQuery.rollingNumber.js
+++ jQuery.rollingNumber.js
@@ -18,40 +18,77 @@
 			'dataType' : 'url',//url ,local
 			'url' : 'http://www.youwebsite.com/api/',//url ,local
 			'intervalTime' : '3',//间隔时间
-			'numberSize' : '3',//数字位数
+			'number' : 123,//数字位数
 		};
+
+		//设置数字
+		//number数字，如果number用0-9,还可以传第二个参数表示第几位(从左数)
+		this.setNumber = function(newNum){
+			if(newNum < 0){
+				console.error('"newNum" is not a positive number');
+				return ;
+			}
+			if(newNum < 10 && arguments.length > 1){
+				console.log(arguments);
+				var n = arguments[1];
+				var v = arguments[0];
+				var top_px = setItemTop(v);
+				this.el.find('div.rollingNumber_item').eq(n).css('top', top_px);
+			}else{
+				var numberString = newNum.toString();
+				console.log(numberString);
+				var pre0count = this.options.numberSize - numberString.length;
+				numberString = (function(prec){
+					var result = '';
+					for(var i = 0; i < prec; i++){
+						result += '0';
+					}
+					return result;
+				})(pre0count) + numberString;
+				console.log()
+				for(var i = 0; i < numberString.length; i++){
+					this.setNumber(numberString[i], i);
+				}
+			}
+		};
+
 		this.options = $.extend(false,defaultOpts, opts);
-		this.init(el, this.options);
-	};
-	RollingNumber.prototype = {
-		init: function(el, options){
-			var allNumbers = this._initAllNumber(options.numberSize);
+		this.options.numberSize = this.options.number.toString().length;
+		this.el = el;
+		init(el, this.options);
+		this.setNumber(12);
+		function init(el, options){
+			var allNumbers = _initAllNumber(options.numberSize);
 			for(var e in allNumbers){
 				el.append(allNumbers[e]);
 			}
-		},
-		_initAllNumber: function(numberSize){
+		};
+		function _initAllNumber(numberSize){
 			var numbers = new Array();
 			for(var i = 0; i < numberSize; i++){
-				numbers.push(this._initNumber());
+				numbers.push(_initNumber());
 			}
 			return numbers;
-		},
-		_initNumber: function(){
-			var mataEls = this._initMetaNumber();
+		};
+		function _initNumber(){
+			var mataEls = _initMetaNumber();
 			var bitElDiv = $('<div class="rollingNumber_item">' + mataEls + '</div>');
-			bitElDiv.css({'width':this.options.width});
+			bitElDiv.css({'width':that.options.width});
 			var bitEl = $('<span class="rollingNumber_bit"></span>')
-				.html(bitElDiv);
+			.html(bitElDiv);
 			return bitEl;
-		},
+		};
 		//生成没个位置的全部数字0-9
-		_initMetaNumber: function(){
+		function _initMetaNumber(){
 			var result = '';
 			for(var i = 0; i < 10; i++){
 				result += '<span style="display:block;">'+ i + '</span>';
 			}
 			return result;
-		}
-	};
+		};
+		function setItemTop(n){
+			var height = $('.rollingNumber_item>span').height();
+			return (-n * height).toString() + 'px';
+		};
+	}
 })(jQuery);
